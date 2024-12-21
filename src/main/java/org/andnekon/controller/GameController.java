61032709abac0e8c37f074132d566deb67cfd76f
AndreiNekrasOn <an-32010@yandex.ru>
2024-12-21 19:32:10 +0300

    refactor: remove controller inheritance ieararchy

--- src/main/java/org/andnekon/controller/GameController.java
+++ src/main/java/org/andnekon/controller/GameController.java
@@ -1,10 +1,35 @@
 package org.andnekon.controller;
 
-// TODO: controller should be responsible for translating input into
-// sensible action? GameLogic - observer
-public interface GameController {
+import org.andnekon.game.GameLogic;
+import org.andnekon.game.state.State.Type;
+import org.andnekon.view.GameView;
+import org.andnekon.view.Reader;
 
-    public void run(String line);
+public class GameController {
 
-    public void run();
+    protected GameLogic game;
+
+    protected GameView view;
+
+    protected Reader reader;
+
+    public GameController(GameLogic game, GameView view, Reader reader) {
+        this.game = game;
+        this.view = view;
+        this.reader = reader;
+    }
+
+    public void run() {
+        view.welcome();
+        String in;
+        do {
+            view.display(game.getCurrentState());
+            in = reader.read();
+            processInput(in);
+        } while (game.getCurrentState().getType() != Type.QUIT);
+    }
+
+    public void processInput(String in) {
+        game.handleInput(in);
+    }
 }
--- src/main/java/org/andnekon/controller/GameControllerFactory.java
+++ src/main/java/org/andnekon/controller/GameControllerFactory.java
@@ -1,6 +1,12 @@
 package org.andnekon.controller;
 
 import org.andnekon.game.GameLogic;
+import org.andnekon.view.GameView;
+import org.andnekon.view.Reader;
+import org.andnekon.view.raw.ConsoleRawView;
+import org.andnekon.view.repl.ConsoleReader;
+import org.andnekon.view.repl.ConsoleView;
+import org.andnekon.view.tui.TuiView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,10 +23,24 @@ public class GameControllerFactory {
     public static GameController createController(
             ControllerType type, InputStream is, OutputStream os) throws IOException {
         GameLogic game = new GameLogic();
-        return switch (type) {
-            case RAW -> new RawController(game, is, os);
-            case REPL -> new ReplController(game);
-            case TUI -> new TuiController(game, is, os);
-        };
+        GameView view;
+        Reader reader;
+        switch (type) {
+            case RAW -> {
+                view = new ConsoleRawView(game.getSession(), is, os);
+                reader = (ConsoleRawView) view;
+            }
+            case REPL -> {
+                view = new ConsoleView(game.getSession());
+                reader = new ConsoleReader(); // TODO: IO
+            }
+            case TUI -> {
+                view = new TuiView(game.getSession(), is, os);
+                reader = (TuiView) view;
+            }
+            default -> throw new UnsupportedOperationException("ControllerType not implemented");
+        }
+        ;
+        return new GameController(game, view, reader);
     }
 }
