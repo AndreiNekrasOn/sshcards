fabc013ab666cbd7d562cef20f705cc255a678bf
AndreiNekrasOn <an-32010@yandex.ru>
2024-12-25 01:43:47 +0300

    feat: maven profile for standalone tui for easier debugging

---
+++ src/main/java/org/andnekon/server/StandaloneTui.java
@@ -0,0 +1,22 @@
+package org.andnekon.server;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
+import org.andnekon.controller.GameController;
+import org.andnekon.controller.GameControllerFactory;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class StandaloneTui {
+
+    public static void main(String[] args) throws IOException {
+        Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.ERROR);
+        GameController controller =
+                GameControllerFactory.createController(
+                        GameControllerFactory.ControllerType.TUI, System.in, System.out);
+        controller.run();
+    }
+}
